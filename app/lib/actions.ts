7cd5e49900484d69a52f10ba4e845ec097fd1996
fiskhandlarn <oskar@joelson.org>
2024-01-04 16:07:09 +0100

    feat: gather params from form action

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,6 +1,11 @@
 'use server';
 
 export async function createInvoice(formData: FormData) {
-  console.log('createInvoice', formData);
+  const rawFormData = {
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  };
 
+  console.log('createInvoice', rawFormData);
 }
