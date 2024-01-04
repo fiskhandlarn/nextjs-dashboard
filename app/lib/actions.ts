7716e8fc2af1960bd801206c81a8cf07c8800837
fiskhandlarn <oskar@joelson.org>
2024-01-04 16:14:42 +0100

    feat: format amount for db, create date

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -13,11 +13,14 @@ const FormSchema = z.object({
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
 
 export async function createInvoice(formData: FormData) {
-  const rawFormData = CreateInvoice.parse({
+  const { customerId, amount, status } = CreateInvoice.parse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),
   });
 
-  console.log('createInvoice', rawFormData);
+  const amountInCents = amount * 100;
+  const date = new Date().toISOString().split('T')[0];
+
+  console.log('createInvoice', customerId, amount, status, amountInCents, date);
 }
